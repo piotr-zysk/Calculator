1532c4b389dfa8a21c9c14593cff9a378a9d796a
Piotr Zysk <pizy123@gmail.com>
2019-04-06 22:42:24 +0200

    Unit tests improved.

--- Calculator.UnitTests/CalculationsShould.cs
+++ Calculator.UnitTests/CalculationsShould.cs
@@ -5,16 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Calculator;
+using Xunit.Sdk;
+using System.Reflection;
 
 namespace Calculator.UnitTests
 {
+    class FindPatternDataAttribute : DataAttribute
+    {
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            yield return new object[] { "0.3333333", "3" };
+            yield return new object[] { "1.1111111", "1" };
+            yield return new object[] { "1.1111112", "" };
+            yield return new object[] { "1.0101010", "01" };
+            yield return new object[] { "1.12312312", "123" };
+            yield return new object[] { "1.78978978979", "789" };
+        }
+    }
+
     public class CalculationsShould
     {
-        [Fact]
-        public void GetByLogint()
+        [Theory]
+        [FindPatternData]
+        public void FindPattern(string inputData, string expectedResult)
         {
-            Pattern pattern = new Calculations().FindPattern("0.333333");
-            Assert.Equal("01", pattern?.Value);
+            Pattern pattern = new Calculations().FindPattern(inputData);
+            Assert.Equal(expectedResult, pattern?.Value);
         }
     }
 }
